448ae871704e041c94e253cafcbd9ab1a02555bb
Gerald Kaszuba <gak@gak0.com>
2015-11-21 09:11:24 +1100

    connected runrem-api entry point

--- web/entry_points.py
+++ web/entry_points.py
@@ -1,9 +1,22 @@
+import os
+
+from django.core.management import call_command
+from django.core.wsgi import get_wsgi_application
+
 from web.celery import app
 
 
 def runner():
     argv = [
         'worker',
-        '--loglevel=DEBUG',
+        '--loglevel=INFO',
+        '-c 1',
     ]
     app.worker_main(argv)
+
+
+def api():
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'web.settings')
+
+    application = get_wsgi_application()
+    call_command('runserver', '127.0.0.1:8000')
